5684365562c9e812909f51cac3dccc58685dccd1
Vitamin-JS <Vpopchenkov@gmail.com>
2020-11-18 01:38:34 +0200

    Add html, Pnotify, folder's architecture  and files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import './styles.css';
-import menuTemplate from './template.hbs';
-
-
+import './css/styles.css';
+import fetchCountriesAPI from './js/fetchCountries.js';
+import countryMarkup from './hbs/country.hbs';
+import coumtriesListMarkup from './hbs/countries-list.hbs';
+import debounce from 'lodash.debounce';
+import { info, error } from '@pnotify/core';
